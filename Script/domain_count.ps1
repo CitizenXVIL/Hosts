490214480639bc521e245aabebb67aaafed66f03
CitizenXVIL <33468864+CitizenXVIL@users.noreply.github.com>
2018-05-25 18:19:03 -0400

    Add files via upload
    Script to count number of domains for future wildcard generating

---
+++ Script/domain_count.ps1
@@ -0,0 +1,28 @@
+﻿$hosts = Get-Content('hosts.txt')
+
+$countlist = New-Object System.Collections.Generic.List[System.Object]
+
+Write-Output 'Determining parent domains for list...'
+
+$hosts | ForEach-Object {
+
+    if ($_.split("\.")[-2] -ne "com" -and
+        $_.split("\.")[-2] -ne "co" -and
+        $_.split("\.")[-2] -ne "in" -and
+        $_.split("\.")[-2] -ne "gov" -and
+        $_.split("\.")[-2] -ne "net" -and
+        $_.split("\.")[-2] -ne "edu" -and
+        $_.split("\.")[-2] -ne "go") {
+            $split = $_.split("\.")[-2,-1] -join "."
+            }
+            else
+            {
+            $split = $_.split("\.")[-3,-2,-1] -join "."
+    }
+
+$countlist.Add($split)
+}
+
+Write-Output 'Counting number of occurences of each parent domain (this will take several minutes)...'
+
+Write-Output $countlist | group | where-object { $_.count -gt 10 } | Sort Count -Descending | ft Name,Count
